bf6bce7f7e8b0d2418ffd1d1bc67da3206236e7d
aldair burgos <aldairburgos2@gmail.com>
2024-08-25 15:39:22 -0600

    agregamos el switch de la box

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,31 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Operaciones'),
+        title: Text('Operaciones',
+      style: TextStyle(color: Colors.white)
+        ),
+        backgroundColor:Color.fromARGB(255, 50, 10, 230),
       ),
-         );
+      home:calculadoraPage();
+    );
   }
+}
+
+class CalculadoraPage extends StatefulWidget{
+  @override
+  _calculadoraPage createState() = > _calculadoraPage(),
+  }
+
+class _CalculadoraPageState extends State<CalculadoraPage> {
+  final TextEditingController _num1Controller = TextEditingController();
+  final TextEditingController _num2Controller = TextEditingController();
+  String _selectedOperation = 'Suma';
+  String _resultado = '';
+
+ void _calculate() {
+    final double num1 = double.tryParse(_num1Controller.text) ?? 0;
+    final double num2 = double.tryParse(_num2Controller.text) ?? 0;
+    double result;
+ }
+
 }
